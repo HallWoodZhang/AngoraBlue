471cb859223d47e0456b44b38cf0822fef6bb2f3
HallWoodZhang <hallwood_zhang@163.com>
2017-09-06 16:40:05 +0800

    update .gitignore

--- ans/BinasciiUtils.py
+++
@@ -1,8 +0,0 @@
-import binascii
-
-
-def fourCharsToInt(s):
-    return int(binascii.hexlify(s), 16)
-
-def intToFourChars(i):
-    return binascii.unhexlify(format(i, 'x'))
--- ans/PyInstallerUtils.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-
-def resourcePath(relativePath):
-    basePath = getattr(sys, '_MEIPASS', os.path.abspath('.'))
-    return os.path.join(basePath, relativePath)
